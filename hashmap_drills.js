5c2fec454b1d64d36fc798fd6024ca0cc444b65b
Michael <mmbmf.mm@gmail.com>
2020-03-10 10:10:34 -0500

    core functions & initial drills complete

--- hashmap_drills.js
+++ hashmap_drills.js
@@ -6,6 +6,19 @@ const main = () => {
   lotr.SIZE_RATIO = 3;
 
   lotr.set("Hobbit", "Bilbo");
+  lotr.set("Hobbit", "Frodo");
+  lotr.set("Wizard", "Gandalf");
+  lotr.set("Human", "Aragorn");
+  lotr.set("Elf", "Legolas");
+  lotr.set("Maiar", "The Necromancer");
+  lotr.set("Maiar", "Sauron");
+  lotr.set("RingBearer", "Gollum");
+  lotr.set("LadyOfLight", "Galadriel");
+  lotr.set("HalfElven", "Arwen");
+  lotr.set("Ent", "Treebeard");
+
+  console.log(lotr.get("Maiar"));
+  console.log(lotr.get("Hobbit"));
 
   return lotr;
 };
